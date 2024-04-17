4964be8970738bf23e8fb1ae25f86a45d5872812
Piotr Detyna <piotrek.detyna@gmail.com>
2024-04-17 19:24:48 +0200

    Improved Python solution

--- 11-agenci/solution.py
+++ 11-agenci/solution.py
@@ -1,30 +1,43 @@
 from collections import defaultdict, deque
 
-def bfs(graph, start):
-    queue = deque([(start[0], start[1], 1, 1)]) 
-    visited = set((start))
-    
-    while queue:
-        agent1, agent2, steps, turn = queue.popleft()
-        
-        if agent1 == agent2:
-            return steps 
-        
-        if turn == 1:
-            for new_agent1_position in graph[agent1]:
-                state = (new_agent1_position, agent2)
-                if state not in visited:
-                    visited.add(state)
-                    queue.append((new_agent1_position, agent2, steps + 1, 2))
-        else:
-            for new_agent2_position in graph[agent2]:
-                state = (agent1, new_agent2_position)
-                if state not in visited:
-                    visited.add(state)
-                    queue.append((agent1, new_agent2_position, steps + 1, 1))
+def bfs(graph, a1, a2):
+    queue_after_agent2_move = deque([(a1, a2)])
 
-    
-    return 
+    visited_after_agent1_move = set()
+    visited_after_agent2_move = set([(a1, a2)])
+
+    days = 0
+    while queue_after_agent2_move:
+        queue_after_agent1_move = deque()
+
+        #ruch agenta1
+        while queue_after_agent2_move:
+            agent1, agent2 = queue_after_agent2_move.popleft()
+            if agent1 == agent2:
+                return days
+            
+            # przechodzimy po sasiednich miastach agenta1
+            # dodajemy nowe stany po przemieszczeniu sie agenta1
+            for new_agent1 in graph[agent1]:
+                state = (new_agent1, agent2)
+                if not state in visited_after_agent1_move:
+                    visited_after_agent1_move.add(state)
+                    queue_after_agent1_move.append(state)
+        
+        # ruch agenta2
+        while queue_after_agent1_move:
+            agent1, agent2 = queue_after_agent1_move.popleft()
+
+            # przechodzimy po sasiednich miastach agenta2
+            # dodajemy nowe stany po przemieszczeniu sie agenta2
+            for new_agent2 in graph[agent2]:
+                state = (agent1, new_agent2)
+                if not state in visited_after_agent2_move:
+                    visited_after_agent2_move.add(state)
+                    queue_after_agent2_move.append(state)
+
+        days += 1
+    return 'NIE'
 
 n, m = map(int, input().split())
 agent1, agent2 = map(int, input().split())
@@ -34,5 +47,6 @@ for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
 
-res = bfs(graph, (agent1, agent2))
-print(res // 2)
+res = bfs(graph, agent1, agent2)
+print(res)
+
